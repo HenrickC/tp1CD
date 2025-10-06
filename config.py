1335173d73eac0015b6a561ec27126e0523f7e8e
HenrickC <106536163+HenrickC@users.noreply.github.com>
2025-10-06 11:23:01 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+# Cole sua chave da API do YouTube v3 aqui
+# É fundamental manter este arquivo seguro e não compartilhá-lo publicamente
+YOUTUBE_API_KEY = "AIzaSyCQisfPbWJm-fS5DXIY31k5zZDrafyyD5o"
